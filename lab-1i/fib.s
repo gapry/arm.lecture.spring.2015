2569f8efb637424566162a431a597526e84a32ed
gapry <gapryun@gmail.com>
2015-03-03 18:24:47 +0800

    [lab-1i]: version-1

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -4,40 +4,54 @@
 	.align 2
 	.thumb
 	.thumb_func
-
 	.global fibonacci
 	.type fibonacci, function
-
 fibonacci:
-	@ ADD/MODIFY CODE BELOW
-	@ PROLOG
-	push {r3, r4, r5, lr}
+	cmp r0, #1
+	ble .L0
+	mov r3, #1	@counter
+	mov r1, #0	@cache Fn-2
+	mov r2, #1	@cache Fn-1
+.LOOP:	
+	add r1, r1, r2
+	/* Overflow Detection */
+	/*----------------------------------*/	
+	mrs r4, CPSR
+	mov r4, r4, LSR #26
+	and r4, #00001
+	cmp r4, #1
+	beq .EXIT
+	/*----------------------------------*/	
+	add r3, #1
+	cmp r3, r0
+	beq .L1
+	/*----------------------------------*/	
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
-
-	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
-
+	add r2, r1, r2
+	/* Overflow Detection */
+	/*----------------------------------*/	
+	mrs r4, CPSR
+	mov r4, r4, LSR #26
+	and r4, #00001
+	cmp r4, #1
+	beq .EXIT
+	/*----------------------------------*/	
+	mrs r4, CPSR
+	add r3, #1
+	cmp r3, r0
+	beq .L2
+	/*----------------------------------*/	
+	b .LOOP
+.L0:
+	bx lr
+.L1:
+	mov r0, r1
+	bx lr
+.L2:
+	mov r0, r2
+	bx lr
+.EXIT:
+	mov r0, #-1
+	bx lr
 	.size fibonacci, .-fibonacci
 	.end
