d0ed946bb7ac1c583a4cec6cd9bfb7b1cd0e7a60
gapry <gapry.tw@gmail.com>
2015-03-13 03:54:02 +0800

    [lab-1i]: Convert to lowercase.

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -3,29 +3,29 @@
 	.text
 	.global fibonacci
 	.type fibonacci, function
-@ ARM ISA
+@ arm isa
 fibonacci:
     .code 32
-    CMP r0, #1
-    BXLE lr
-    MOV r2, r0
-    LDR r0, =.THUMB_MODE+1
-    BX r0
-@ THUMB ISA
+    cmp r0, #1
+    bxle lr
+    mov r2, r0
+    ldr r0, =.thumb_mode+1
+    bx r0
+@ thumb isa
     .code 16
-.THUMB_MODE:
-    MOV r0, #0  @fn-2
-    MOV r1, #1  @fn-1
-.LOOP:
-    ADDS r0, r1
-    ITT VS          @Overflow Detection
-    MOVVS r0, -1
-    BLXVS lr
-    EORS r0, r1
-    EORS r1, r0
-    EORS r0, r1
-    SUBS r2, #1
-    BGT .LOOP
-    BX lr
+.thumb_mode:
+    mov r0, #0  @fn-2
+    mov r1, #1  @fn-1
+.loop:
+    adds r0, r1
+    itt vs          @overflow detection
+    movvs r0, -1
+    blxvs lr
+    eors r0, r1
+    eors r1, r0
+    eors r0, r1
+    subs r2, #1
+    bgt .loop
+    bx lr
 	.size fibonacci, .-fibonacci
 	.end
