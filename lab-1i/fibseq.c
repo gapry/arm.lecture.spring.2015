494331425d5f0f97ef329068eab552a1f56b70d8
gapry <gapry.tw@gmail.com>
2015-03-17 06:44:48 +0800

    [lab38]: Add the measure feature.

--- lab-1i/fibseq.c
+++ lab-1i/fibseq.c
@@ -1,18 +1,45 @@
 #include <stdio.h>
+#include <time.h>
 
 extern int fibonacci(int x);
+
+struct timespec diff(struct timespec start, struct timespec end)
+{
+    struct timespec temp;
+    if ((end.tv_nsec-start.tv_nsec)<0) {
+        temp.tv_sec = end.tv_sec-start.tv_sec-1;
+        temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+    } else {
+        temp.tv_sec = end.tv_sec-start.tv_sec;
+        temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+    }
+    return temp;
+}
+
+void reset(struct timespec* start, struct timespec* end)
+{
+    (*start).tv_sec = 0;
+    (*end).tv_nsec = 0;
+}
 
 int main(int argc, char **argv)
 {
     int number=0;
     int result=0;
-
+    struct timespec time_start, time_end;
+   
     scanf("%d",&number);
-    result = fibonacci(number);   
+  
+    reset (&time_start, &time_end);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time_start); 
+    result = fibonacci(number);  
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time_end); 
     if(result != -1) {
+        printf("Time is %lu(s) : %lu(ns)\n", diff (time_start, time_end).tv_sec, diff (time_start, time_end).tv_nsec);
         printf("The fibonacci sequence at %d is: %d\n", number, result);
     } else {
         printf("It occurs overflow.\n");
     }
+    reset (&time_start, &time_end);
 }
 
--- lab-1i/run.sh
+++ lab-1i/run.sh
@@ -1,1 +1,10 @@
-make clean && make && make qemu 
+#!/bin/bash
+
+make clean && make 
+
+c=0
+while [ $c -le 45 ]
+do
+    qemu-arm -L /usr/arm-linux-gnueabihf ./fibseq <<< $c
+    (( c++ ))
+done
